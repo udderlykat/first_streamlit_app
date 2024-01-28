228393c2ad5771a7e9414e62684ec00f95895855
udderlykat <157666090+udderlykat@users.noreply.github.com>
2024-01-28 18:32:06 -0500

    Update streamlit_app.py
    change header to double quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
-streamlit.header('Fruityvice Fruity Advice!')
+streamlit.header("Fruityvice Fruity Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
